0f9c677af8472e75ebb2eadadc3f07791d81b3d4
Роман Мстиславский <mstislavskijr@gmail.com>
2025-05-30 21:10:20 +0300

    add modul processing

--- src/masks.py
+++ src/masks.py
@@ -6,7 +6,7 @@ def get_mask_card_number(card_number: Union[str]) -> str:
     """Функция которая принимает на вход номер карты и возвращает ее маску"""
 
     card_number = card_number.replace(" ", "")
-    masked_card_number = " ".join(card_number[i:i + 4] for i in range(0, len(card_number), 4))
+    masked_card_number = " ".join(card_number[i : i + 4] for i in range(0, len(card_number), 4))
     masked_card_number_list = list(masked_card_number)
 
     for i in range(len(masked_card_number_list)):
---
+++ src/processing.py
@@ -0,0 +1,34 @@
+def filter_by_state(list_inp: list[dict], state="EXECUTED") -> list[dict]:
+    new_list = []
+    for i in list_inp:
+        if i["state"] == state:
+            new_list.append(i)
+    return new_list
+
+
+print(
+    filter_by_state(
+        [
+            {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
+            {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
+            {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
+            {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
+        ]
+    )
+)
+
+
+def sort_by_date(base_idtime: list[dict], reverse=False) -> list[dict]:
+    return sorted(base_idtime, key=lambda x: x["date"], reverse=reverse)
+
+
+print(
+    sort_by_date(
+        [
+            {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
+            {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
+            {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
+            {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
+        ]
+    )
+)
