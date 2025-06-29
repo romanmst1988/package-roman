98cc2b2446632866ec1e267cc6ae5dde45e30f90
Роман Мстиславский <mstislavskijr@gmail.com>
2025-06-29 17:23:34 +0300

    Module utils and directory data created

--- main.py
+++ main.py
@@ -1,17 +1,5 @@
-# from src.masks import get_mask_account, get_mask_card_number
-#
-# print(get_mask_card_number("1234567812345678"))
-#
-# print(get_mask_account("123456"))
-#
+from src.masks import get_mask_account, get_mask_card_number
 
+print(get_mask_card_number("1234567812345678"))
 
-import json
-
-# data - строка, тип str
-data = '{"name": "John Smith", "age": 30, "city": "New York"}'
-# parsed_data - словарь, тип dict
-parsed_data = json.loads(data)
-
-# Получаем значение словаря по ключу
-print(parsed_data["name"], parsed_data["age"], parsed_data["city"])
+print(get_mask_account("123456"))
