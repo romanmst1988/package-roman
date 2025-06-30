32993616d6ab15b28e7c739db3da57af7c9908e7
Роман Мстиславский <mstislavskijr@gmail.com>
2025-06-30 19:17:19 +0300

    modifi poetry

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-# from src.masks import get_mask_account, get_mask_card_number
-#
-# print(get_mask_card_number("1234567812345678"))
-#
-# print(get_mask_account("123456"))
+from src.masks import get_mask_account, get_mask_card_number
+
+print(get_mask_card_number("1234567812345678"))
+
+print(get_mask_account("123456"))
+
+
